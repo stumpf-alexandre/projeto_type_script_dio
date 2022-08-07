f00dacaccb761e560604ce4d8ba39c9886f1946f
stumpf-alexandre <als.stumpf@gmail.com>
2022-08-07 14:26:19 -0300

    criando uma função de soma com type script

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log('Type Script');
+//console.log('Type Script');
+
+//criando uma função de soma com type script
+function soma(a: number, b: number){
+    return a + b;
+}
+
+console.log(soma(3, 12));

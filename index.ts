96ad84c541e6617286fdc5d06a373f48d75eb9bb
stumpf-alexandre <als.stumpf@gmail.com>
2022-08-07 15:25:01 -0300

    executando interfaces em type scripts

--- index.ts
+++ index.ts
@@ -1,8 +1,32 @@
 //console.log('Type Script');
 
+
 //criando uma função de soma com type script
 function soma(a: number, b: number){
     return a + b;
 }
 
 console.log(soma(3, 12));
+
+//interfaces
+interface IAnimal{
+    nome: string;
+    tipo: 'terrestre' | 'aquático';
+    executarRUgido(alturaEmDecibeis: number): void;
+}
+const animal: IAnimal = {
+    nome: 'Elefante',
+    tipo: 'terrestre',
+    executarRUgido: (alturaEmDecibeis) => ('${alturaEmDecibeis}dB'),
+}
+
+animal.executarRUgido(38);
+
+interface IFelino extends IAnimal{
+    visaoNoturna: boolean;
+}
+const felino: IFelino = {
+    nome: 'Leão',
+    tipo: 'terrestre',
+    visaoNoturna: true,
+}
